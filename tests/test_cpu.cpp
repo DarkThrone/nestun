19f940251aa2eb42dd7c5b510b793e1f29a35095
DarkThrone <geronimo@fastmail.com>
2026-06-10 23:53:07 +1000

    Mas tests

---
+++ tests/test_cpu.cpp
@@ -0,0 +1,17 @@
+#include "doctest.h"
+
+TEST_CASE("Addressing Modes") {
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+  SUBCASE("ZeroPage") { CHECK(1 + 1 == 2); }
+}
--- tests/test_example.cpp
+++
@@ -1,5 +0,0 @@
-#include "doctest.h"
-
-TEST_CASE("example") {
-    CHECK(1 + 1 == 2);
-}
